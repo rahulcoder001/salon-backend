020179837a69af2c3fe788869c598f4b33aaa891
rahulcoder001 <124455574+rahulcoder001@users.noreply.github.com>
2025-04-04 16:31:28 +0530

    forget password()

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { userLogin , userSignup} = require("../controllers/userController");
+const { userLogin , userSignup, changePassword} = require("../controllers/userController");
 
 const router = express.Router();
 
 // User Login Route
 router.post("/signup", userSignup)
 router.post("/login", userLogin);
-
+router.post("/forgetpassword", changePassword)
 
 module.exports = router;
