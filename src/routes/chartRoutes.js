fd14945e5426e27891de65e668b8f2023d21eb09
rahulcoder001 <124455574+rahulcoder001@users.noreply.github.com>
2025-04-14 19:27:56 +0530

    add ataff

--- src/routes/chartRoutes.js
+++ src/routes/chartRoutes.js
@@ -3,6 +3,6 @@ const { getDailyStatsLast30Days } = require("../controllers/chartControllers");
 
 const router = express.Router();
 
-router.get("/chartthree/:salonId",getDailyStatsLast30Days)
+router.get("/three/:salonId",getDailyStatsLast30Days)
  
 module.exports = router;
