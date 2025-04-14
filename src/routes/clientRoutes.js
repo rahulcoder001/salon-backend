25e90f444a659b5144ad77fec605ba3a620877a4
satyam-svg <satyammaurya9620@gmail.com>
2025-04-14 15:18:04 +0530

    adding branch management

--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const { getRecentClientsCount } = require("../controllers/clientscontrollers");
 
 
 const router = express.Router();
 
- 
+router.get("/totalclients/:salonId",getRecentClientsCount);
 module.exports = router;
