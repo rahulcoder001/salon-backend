9051aa5920fd921e03d0d355e4d2992af0829303
rahulcoder001 <124455574+rahulcoder001@users.noreply.github.com>
2025-04-19 12:27:57 +0530

    add ataff

--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { getRecentClientsCount, addClient } = require("../controllers/clientscontrollers");
+const { getRecentClientsCount, addClient, getClientsBySalon } = require("../controllers/clientscontrollers");
 
 
 const router = express.Router();
 
 router.get("/totalclients/:salonId",getRecentClientsCount);
 router.post("/addclients",addClient);
+router.get("/gettotalclient/:salonId", getClientsBySalon)
 module.exports = router;
