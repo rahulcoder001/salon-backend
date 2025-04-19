339abbf9773e13f140c799badbd0386f3ec45e6b
satyam-svg <satyammaurya9620@gmail.com>
2025-04-19 13:29:18 +0530

    added

--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get("/totalclients/:salonId",getRecentClientsCount);
 router.post("/addclients",addClient);
-router.get("/gettotalclient/:salonId", getClientsBySalon);
+router.get("/gettotalclient/:salon_id", getClientsBySalon);
 router.put("/updateclient/:id", updateClient);
 router.delete("/deleteclient/:id", deleteClient);
 module.exports = router;
