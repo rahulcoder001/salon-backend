455ceec3f9a3e20a7a977e8811eba6bcc0bacc98
satyam-svg <satyammaurya9620@gmail.com>
2025-04-21 03:12:02 +0530

    added salary tble

--- src/controllers/branchControllers.js
+++ src/controllers/branchControllers.js
@@ -1,38 +1,6 @@
 const prisma = require("../config/db");
 
-// **Add Branch**
-const addBranch = async (req, res) => {
-  const { branch_name, branch_location, salon_id , contact_email,contact_number,opning_time,closeings_time} = req.body;
 
-  try {
-    // Check if the salon exists
-    const existingSalon = await prisma.salon.findUnique({
-      where: { id: salon_id },
-    });
-
-    if (!existingSalon) {
-      return res.status(404).json({ message: "Salon not found" });
-    }
-
-    // Create new branch
-    const newBranch = await prisma.branch.create({
-      data: {
-        branch_name,
-        branch_location,
-        salon_id,
-        contact_email,
-        contact_number,
-        opning_time,
-        closeings_time
-      },
-    });
-
-    res.status(201).json({ message: "Branch created successfully", branch: newBranch });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server error" });
-  }
-};
 
 
 
@@ -89,4 +57,58 @@ const IsBranch = async (req, res) => {
   }
 };
 
-module.exports = { addBranch , IsBranch };
+
+
+// Add Branch
+const addbrnch = async (req, res) => {
+  try {
+    const { 
+      branch_name, 
+      branch_location, 
+      contact_email, 
+      contact_number, 
+      opning_time, 
+      closeings_time, 
+      salon_id 
+    } = req.body;
+
+    const newBranch = await prisma.branch.create({
+      data: {
+        branch_name,
+        branch_location,
+        contact_email,
+        contact_number,
+        opning_time,
+        closeings_time,
+        salon_id
+      }
+    });
+
+    res.status(201).json(newBranch);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+// Update Branch
+const updatebranch =  async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updateData = req.body;
+
+    const updatedBranch = await prisma.branch.update({
+      where: { id },
+      data: updateData
+    });
+
+    res.json(updatedBranch);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+
+
+module.exports = { addbrnch , IsBranch , updatebranch };
