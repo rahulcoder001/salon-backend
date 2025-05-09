dbf540776b08cdd6f775944472c22157c11598f3
satyam-svg <satyammaurya9620@gmail.com>
2025-05-09 12:19:02 +0530

    added tables in schema

--- src/controllers/smscompaignController.js
+++ src/controllers/smscompaignController.js
@@ -1,5 +1,5 @@
-const accountSid = 'AC4017ea8c0cd291a18db4f12752c84d94';
-const authToken = '8bf87077c89b81461f86c88fc168842e';
+const accountSid = '';
+const authToken = '';
 const client = require('twilio')(accountSid, authToken);
 
 const sendSMS = async (req, res) => {
