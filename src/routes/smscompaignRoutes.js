6512442381808f447e9f9539398114abcf1da9a5
satyam-svg <satyammaurya9620@gmail.com>
2025-05-09 12:20:11 +0530

    added tables in schema

--- src/routes/smscompaignRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { sendSMS } = require("../controllers/smscompaignController");
-
-const router = express.Router();
-
-router.post('/send-sms', sendSMS);
-
-module.exports = router;
